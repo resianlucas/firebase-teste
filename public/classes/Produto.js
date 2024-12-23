b0a1bd4066c7eafbdd8eddfb207ce8e42c625ee5
resianlucas <rm95781@fiap.com.br>
2024-12-23 09:32:27 -0300

    alterando tag produto.midia.imagens.imagensURL

--- public/classes/Produto.js
+++ public/classes/Produto.js
@@ -1564,7 +1564,7 @@ export async function criarProduto(produto) {
             },
             midia: {
                 imagens: {
-                    imagemURL: [
+                    imagensURL: [
                         {
                             link: produto.imageUrl
                         }
